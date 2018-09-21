3260be94eafdcb4140cf5b5a73d5085ec922e69c
renanhangai <hangai.renan@gmail.com>
2018-09-21 15:19:05 -0300

    fix: PHP generation was not treating null and undefined as expected

--- src/Writer.js
+++ src/Writer.js
@@ -37,7 +37,9 @@ module.exports = class Writer {
 	static writePHPValue( value, identChar, identLevel ) {
 		identChar  = identChar || "\t";
 		identLevel = identLevel|0;
-		if ( Array.isArray( value ) ) {
+		if ( ( value === null ) || ( value === void(0) ) ) {
+			return 'null';
+		} else if ( Array.isArray( value ) ) {
 			const identSelf = _.repeat( identChar, identLevel );
 			const ident     = identSelf+identChar;
 			return "array(\n" + value.map( (i) => ident + Writer.writePHPValue( i, identChar, identLevel + 1 ) ).join( ",\n" ) + ",\n"+identSelf+")";
@@ -45,10 +47,12 @@ module.exports = class Writer {
 			const identSelf = _.repeat( identChar, identLevel );
 			const ident     = identSelf+identChar;
 			const body      = _.map( value, ( item, key ) => {
+				if ( item === void(0) )
+					return false;
 				key  = JSON.stringify( key );
 				item = Writer.writePHPValue( item, identChar, identLevel + 1 );
 				return ident + key + " => " + item;
-			}).join( ",\n" );
+			}).filter( Boolean ).join( ",\n" );
 			return "array(\n" + body + ",\n"+identSelf+")";
 		}
 		return JSON.stringify( value );
